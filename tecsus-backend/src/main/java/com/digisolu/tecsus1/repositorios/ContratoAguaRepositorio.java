fcf829cec62df3f7ae940fde9b0721380157c882
JulianaMaria-Lab <juliana.ramos01@fatec.sp.gov.br>
2022-05-07 16:40:55 -0300

    Mapeamento:Concessionaria(CNPJ)/Contratos de Água(Num.Forn.)Energia(Cód.Ident.)

--- tecsus-backend/src/main/java/com/digisolu/tecsus1/repositorios/ContratoAguaRepositorio.java
+++ tecsus-backend/src/main/java/com/digisolu/tecsus1/repositorios/ContratoAguaRepositorio.java
@@ -1,9 +1,10 @@
 package com.digisolu.tecsus1.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.digisolu.tecsus1.entidades.ContratoAgua;
 
 public interface ContratoAguaRepositorio extends JpaRepository<ContratoAgua, Long>{
-
+	ContratoAgua findByNumeroFornecimento(@RequestParam("n_fornecimento") String n_fornecimento);
 }
