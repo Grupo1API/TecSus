94408791a01b3f4c70b821225d058590ab5e0f67
Richardrafael <richard.rafael.soares@gmail.com>
2022-05-20 18:08:25 -0300

    sd

--- tecsus-front/tecsus/src/pages/dashboard/index.js
+++ tecsus-front/tecsus/src/pages/dashboard/index.js
@@ -1,12 +1,23 @@
-import './style.css';
-import Menu from '../../components/menu'
+import Grafico from "../../components/dadosUsina/grafico";
+import Menu from "../../components/menu/index";
+import "./style.css";
+
 
 function Dashboard() {
   return (
-    <div >
-      <Menu/>
+    <div>
+      <div className="container">
+        <div className="content">
+          <Menu />
+        </div>
+        <div className="section">
+          <h1>Dashboard</h1>
+          <Grafico />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Dashboard;
+
