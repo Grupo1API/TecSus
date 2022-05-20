a764799fa0eba6ba9c3b3199ba2cccdf1a167193
ciscoquirino <quirino1988@gmail.com>
2022-05-20 15:36:49 -0300

    ajustes no crud usuarios e alteração do App.java para inclusão automatica do usuario admin

--- tecsus-backend/src/main/java/com/digisolu/tecsus1/App.java
+++ tecsus-backend/src/main/java/com/digisolu/tecsus1/App.java
@@ -1,13 +1,34 @@
 package com.digisolu.tecsus1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.digisolu.tecsus1.entidades.Usuarios;
+import com.digisolu.tecsus1.repositorios.UsuariosRepositorio;
+
 @SpringBootApplication
-public class App {
+public class App implements CommandLineRunner {
+	
+	@Autowired
+	private UsuariosRepositorio repositoriodeusuarios;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		Usuarios user1 = new Usuarios();
+		user1.setUsername("admin");
+		user1.setEmail("admin@admin");
+		user1.setSenha("admin");
+		user1.setAcesso(1);
+		
+		repositoriodeusuarios.save(user1);
 	}
 
 }
