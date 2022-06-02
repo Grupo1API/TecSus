c256fbce3f2be172c08eb5d02bd14a63d2d76086
Julio Cesar de Oliveira Martins <julio.martins@codex.com.br>
2022-06-02 11:11:50 -0300

    deploy do backend

--- tecsus-front/tecsus/src/components/rotasProtegidas/index.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth/useAuth";
-
-function RotasProtegidas() {
-  const { token } = useAuth();
-  return token === true ? <Outlet /> : <Navigate to="/login" />;
-}
-
-export default RotasProtegidas;
